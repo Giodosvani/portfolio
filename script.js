5af5f636bdfbe355bd0106038964b4602ccc27f8
Giovani Oliveira <giodosvani123@gmail.com>
2023-01-02 17:32:25 -0300

    light theme added

---
+++ script.js
@@ -0,0 +1,11 @@
+function claro() {
+    document.getElementById('problema').href = "light.css"
+    document.getElementById('escuro').style.display = "block"
+    document.getElementById('claro').style.display = 'none'
+    }
+
+function escuro() {
+    document.getElementById('problema').href = "style.css"
+    document.getElementById('escuro').style.display = "none"
+    document.getElementById('claro').style.display = 'block'
+    }
